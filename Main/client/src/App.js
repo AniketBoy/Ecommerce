f326c70cf2e4609815fc373396d79032cff78af0
AniketBoy <aniketashis93@gmail.com>
2023-11-02 09:42:36 +0530

    3rd

---
+++ Main/client/src/App.js
@@ -0,0 +1,52 @@
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Products from "./pages/Products/Products";
+import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+
+const Layout = () => {
+  return (
+    <div>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/product/:id",
+        element: <Product />
+      },
+      {
+        path: "/products/:id",
+        element: <Products />
+      }
+    ]
+  }
+
+]);
+
+function App() {
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ Main/client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <div className="wrapper">
+                <div className='left'></div>
+                <div className='center'></div>
+                <div className='right'></div>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
